ce86189b33d75d35798539b8b434ba6a384375fa
gabriel <santosgabriel6763@gmail.com>
2023-04-17 11:56:14 -0300

    Update

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,15 @@
-[...document.getElementsByClassName("showcategories")].map(btn => {
-    btn.addEventListener("click", () => {
-        document.getElementById("categories-dropdown-container").classList.toggle("d-none")
+if(document.getElementsByClassName("showcategories").length > 0){
+    [...document.getElementsByClassName("showcategories")].map(btn => {
+        btn.addEventListener("mouseenter", () => {
+            document.getElementById("categories-dropdown-container").classList.toggle("d-none")
+        })
     })
-})
+
+    window.addEventListener("click", () => {
+        document.getElementById("categories-dropdown-container").classList.add("d-none")
+    })
+}
+
 
 const THUMBNAILS = [...document.getElementsByClassName("image-thumbnail")]
 THUMBNAILS.map(thumbnail => {
@@ -10,3 +17,4 @@ THUMBNAILS.map(thumbnail => {
         document.getElementsByClassName("gallery-main-img")[0].src = evt.target.src
     })
 })
+
