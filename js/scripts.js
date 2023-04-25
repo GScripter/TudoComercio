2db263258b88a376447cf36cdbaad044c0cf3963
gabriel <santosgabriel6763@gmail.com>
2023-04-24 23:23:49 -0300

    Update

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,38 @@
 if(document.getElementsByClassName("showcategories").length > 0){
     [...document.getElementsByClassName("showcategories")].map(btn => {
-        btn.addEventListener("click", () => {
-            document.getElementById("categories-dropdown-container").classList.toggle("d-none")
-        })
-        btn.addEventListener("mouseenter", () => {
-            document.getElementById("categories-dropdown-container").classList.toggle("d-none")
-        })
+        if(screen.width <= 768){
+            btn.addEventListener("click", () => {
+                document.getElementById("categories-dropdown-container").classList.toggle("d-none")
+            })
+        }else{
+            btn.addEventListener("mouseenter", () => {
+                document.getElementById("categories-dropdown-container").classList.toggle("d-none")
+            })
+            btn.addEventListener("mouseleave", () => {
+                document.getElementById("categories-dropdown-container").classList.toggle("d-none")
+            })
+        }
     })
 }
 
+if(document.getElementById("categories-dropdown-container")){
+    if(screen.width > 768){
+        document.getElementById("categories-dropdown-container").addEventListener("mouseenter", () => {
+            document.getElementById("categories-dropdown-container").classList.toggle("d-none")
+        })
+        document.getElementById("categories-dropdown-container").addEventListener("mouseleave", () => {
+            document.getElementById("categories-dropdown-container").classList.toggle("d-none")
+        })
+    }
+}
 
 const THUMBNAILS = [...document.getElementsByClassName("image-thumbnail")]
 THUMBNAILS.map(thumbnail => {
     thumbnail.addEventListener("click", (evt) => {
+        THUMBNAILS.map(thumbnails => {
+            thumbnails.style = "border: none;"
+        })
+        evt.target.style = "border: 4px solid #F1A83E;"
         document.getElementsByClassName("gallery-main-img")[0].src = evt.target.src
     })
 })
@@ -23,3 +43,15 @@ if(document.getElementById("btn-filters")){
         document.getElementById("filters").classList.toggle("close")
     })
 }
+
+const THUMBNAILS_WIDTH = document.getElementsByClassName("thumbnails")[0]
+THUMBNAILS.map(thumbnail => {
+    thumbnail.addEventListener("click", (evt) => {
+        evt.target.scrollIntoView({
+            behavior: "smooth",
+            inline: "center",
+            block: "end"
+        })
+    })
+})
+
