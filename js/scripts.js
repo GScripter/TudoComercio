39b020370fdc82de61707a68c327bf57a7e0e4b1
gabriel <santosgabriel6763@gmail.com>
2023-05-05 00:51:59 -0300

    up

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 if(document.getElementsByClassName("showcategories").length > 0){
+    // DropDown of categories from the home page
     [...document.getElementsByClassName("showcategories")].map(btn => {
         if(screen.width <= 768){
             btn.addEventListener("click", () => {
@@ -11,24 +12,55 @@ if(document.getElementsByClassName("showcategories").length > 0){
             btn.addEventListener("mouseleave", () => {
                 document.getElementById("categories-dropdown-container").classList.toggle("d-none")
             })
+            document.getElementById("categories-dropdown-container").addEventListener("mouseenter", () => {
+                document.getElementById("categories-dropdown-container").classList.remove("d-none")
+            })
+            document.getElementById("categories-dropdown-container").addEventListener("mouseleave", () => {
+                document.getElementById("categories-dropdown-container").classList.add("d-none")
+            })
         }
     })
-}
 
-if(document.getElementById("categories-dropdown-container")){
-    if(screen.width > 768){
-        document.getElementById("categories-dropdown-container").addEventListener("mouseenter", () => {
-            document.getElementById("categories-dropdown-container").classList.toggle("d-none")
-        })
-        document.getElementById("categories-dropdown-container").addEventListener("mouseleave", () => {
-            document.getElementById("categories-dropdown-container").classList.toggle("d-none")
-        })
-    }
+    // Do not repeat sample categories from the home page
+    let spaceTopFilled = 0
+    let totalCategoriesVisibleInTheTopRow = -1
+    const SAMPLE_CATEGORIES = document.getElementsByClassName("sample-categories")[0]
+    const SAMPLE_CATEGORIES_CHILDS = [...SAMPLE_CATEGORIES.children]
+    SAMPLE_CATEGORIES_CHILDS.map(el => {
+        if(spaceTopFilled + el.clientWidth < SAMPLE_CATEGORIES.clientWidth){
+            totalCategoriesVisibleInTheTopRow++
+            spaceTopFilled += el.clientWidth
+        }
+    })
+
+    let spaceBottomFilled = 0
+    let totalCategoriesVisibleInTheBottomRow = -1
+    SAMPLE_CATEGORIES_CHILDS.map((el, index) => {
+        if(index > totalCategoriesVisibleInTheTopRow && spaceBottomFilled + el.clientWidth < SAMPLE_CATEGORIES.clientWidth){
+            totalCategoriesVisibleInTheBottomRow++
+            spaceBottomFilled += el.clientWidth
+        }
+    })
+    const totalTopAndBottom = totalCategoriesVisibleInTheTopRow + totalCategoriesVisibleInTheBottomRow
+    const CATEGORIES_DROPDOWN_CHILDS = [...document.getElementById("categories-dropdown-container").children[0].children]
+    CATEGORIES_DROPDOWN_CHILDS.map((el, index) => {
+        if(index-1 <= totalTopAndBottom){
+            document.getElementById("categories-dropdown-container").children[0].innerHTML = ''
+        }else{
+            document.getElementById("categories-dropdown-container").children[0].appendChild(el)
+        }
+    })
 }
 
 const THUMBNAILS = [...document.getElementsByClassName("image-thumbnail")]
 THUMBNAILS.map(thumbnail => {
     thumbnail.addEventListener("click", (evt) => {
+        // Move scroll by clicking thumbnail
+        evt.target.scrollIntoView({
+            behavior: "smooth",
+            inline: "center",
+            block: "nearest"
+        })
         THUMBNAILS.map(thumbnails => {
             thumbnails.style = "border: none;"
         })
@@ -44,14 +76,4 @@ if(document.getElementById("btn-filters")){
     })
 }
 
-const THUMBNAILS_WIDTH = document.getElementsByClassName("thumbnails")[0]
-THUMBNAILS.map(thumbnail => {
-    thumbnail.addEventListener("click", (evt) => {
-        evt.target.scrollIntoView({
-            behavior: "smooth",
-            inline: "center",
-            block: "nearest"
-        })
-    })
-})
 
